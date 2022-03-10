fbe1640e400d9b7336f65577376d5b4c31631101
hoangjam <jameson.hoang@gmail.com>
2022-03-10 10:48:34 -0500

    fadfasdf

---
+++ main.py
@@ -0,0 +1,11 @@
+# James Hoang
+import subprocess
+
+
+# launch game
+def game_launch():
+    subprocess.call([r'E:\SteamLibrary\steamapps\common\Lost Ark\Binaries\Win64\Launch_Game.exe'])
+
+
+if __name__ == '__main__':
+    game_launch()
