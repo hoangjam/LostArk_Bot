ee00557119b881f21caa7a856084655562d15fab
hoangjam <jameson.hoang@gmail.com>
2022-03-10 12:30:43 -0500

    launches game now at least

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import subprocess
 
 # launch game
 def game_launch():
-    subprocess.call([r'E:\SteamLibrary\steamapps\common\Lost Ark\Binaries\Win64\Launch_Game.exe'])
+    # steam://rungameid/1599340
+    subprocess.call(r"C:\Program Files (x86)\Steam\Steam.exe -applaunch 1599340")
 
 
 if __name__ == '__main__':
